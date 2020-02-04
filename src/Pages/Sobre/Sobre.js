b941462d05fc27c64ac7566b87b4f82a381b64e8
Suayder <suayder@uft.edu.br>
2020-02-04 10:33:53 -0300

    final aula 3

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -1,13 +1,37 @@
-import React, { Fragment } from 'react';
-import Header from '../../Components/Header/Header';
+import React, { Fragment } from 'react'
+import Header from '../../Components/Header/Header'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
 
-const Sobre = () =>{
+const useEstilos = makeStyles({
+    titulo: {
+        textAlign: 'center',
+        color: 'blue'
+    }
+})
+
+const Sobre = () => {
+    const classes = useEstilos()
 
     return (
         <Fragment>
             <Header />
-            <h1>Sobre</h1>
+
+            <Container maxWidth='sm'>
+                <Typography
+                    className={classes.titulo}
+                    variant='h1'
+                    component='h2'
+                >
+                    Sobre
+                </Typography>
+                <Typography variant='body1' component='p'>
+                    A Casa do Código é uma editora que desenvolve e edita livros
+                    em diversos formatos.
+                </Typography>
+            </Container>
         </Fragment>
-    );
+    )
 }
-export default Sobre;
+export default Sobre
